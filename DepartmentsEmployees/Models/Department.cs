3ad849b2e858598e05f2b6af40f0078f8c556558
cherkesky <guy@cherkesky.com>
2020-05-12 19:28:06 -0500

    added Department and DepartmentEmployees classes

---
+++ DepartmentsEmployees/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepartmentsEmployees.Models
+{
+    class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+    }
+}
